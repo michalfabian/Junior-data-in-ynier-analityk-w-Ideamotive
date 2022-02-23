2b75da7d713ac3d94b1257e2c7440e909ae71188
michalfabian <y2play@gmail.com>
2022-02-23 16:27:28 +0100

    LM

---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import ssl
+
+_create_unverified_https_context = ssl._create_unverified_context
+ssl._create_default_https_context = _create_unverified_https_context
+
+df = pd.read_json (r'https://data.nasa.gov/resource/gh4g-9sfh.json')
+df.to_csv (r'C:\temp\ladowanie_meteorytow.csv', index=None)
